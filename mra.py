3955fc74c7b715f732b28222a58fda512467143c
Avery B. <126941002+EspiCFH@users.noreply.github.com>
2026-03-28 23:57:53 -0400

    2.0 Source Code Update

--- mra.py
+++ mra.py
@@ -1,344 +1,102 @@
-import requests
+import analysis_functions as analyze
+from analysis_functions import *
 import re
 
-#important variable for determing cause of death
-volatileDOT = ['Bind','Clamp','Fire Spin','Infestation',
-               'Magma Storm','Sand Tomb','Snap Trap','Thunder Cage',
-               'Whirlpool','Wrap','Salt Cure', 'Leech Seed', 'Nightmare',
-               'Curse']
-def pokeList():
-        global p1pL,p2pL
-        #list of just the pokemon
-        p1pL = []
-        p2pL = []
-        p1pLN = re.findall(r'(?<=\|poke\|p1\|).*?(?=[,\|])',rawlog)
-        p2pLN = re.findall(r'(?<=\|poke\|p2\|).*?(?=[,\|])',rawlog)
-        #identify the pokemon's nicknames and such
-        for p in p1pLN:
-            try:
-                p1pL.append((re.findall(r'p1a:\s.+(?=\|%s)' % p,rawlog)[0],p))
-            except:
-                p1pL.append(('p1a: %s' % p,p))
-        
-        for p in p2pLN:
-            try:
-                p2pL.append((re.findall(r'p2a:\s.+(?=\|%s)' % p,rawlog)[0],p))
-            except:
-                p2pL.append(('p2a: %s' % p,p))
+def analyze_replay(link):
+    rawlog = analyze.process(link)
+    analyze.setlog(rawlog)
 
-def kill_analyze():
-    t_index = 0
-    kill_list = []
+    pokelist = analyze.poke_list()
+    p1_list = pokelist[1]
+    p2_list = pokelist[2]
+
+    format_type = analyze.gametype()
+
+    player_names = analyze.pdata()
+
+    p1_hazards = analyze.hazard_list('p1')
+    p2_hazards = analyze.hazard_list('p2')
+
+    analyze.set_hazardlists(p1_hazards[0],p1_hazards[1],p1_hazards[2],p2_hazards[0],p2_hazards[1],p2_hazards[2])
+
+    cumulative_deaths = []
+    cumulative_deaths_rectify = []
     dead_list = []
-    dead_to_rocks = []
-    dead_to_spikes = []
-    dead_to_other = []
-    dead_to_psn = []
-    t_index = 0
-    for t in turnlist:
-        turnspl = t.split('\n')
-        #check for perish song kill
-        if re.search(r'\|-start\|.+perish0',t):
-            perished = re.findall(r'\|-start\|(.+)\|perish0',t)
-            perish_turn = turnlist[t_index-3]
-            perisher = re.findall(r'\|move\|(.+)\|Perish Song',perish_turn)[-1]
-            for d in perished:
-                if d == perisher:
-                    pass
-                else:
-                    kill_list.append(perisher)
-        for x in turnspl:
-            if re.search(r'-damage\|(.+)\|0 fnt',x):
-                dead = re.escape(re.findall(r'\|(p.+)\|0 fnt',x)[0])
-                monplayer = dead[:2]
-                otherp = 'p1' if monplayer == 'p2' else 'p2'
-                #check for rocks kill
-                if re.search(r'%s\|0 fnt\|\[from\]\s(Stealth Rock)' % dead,t):
-                   dead_to_rocks.append((dead,t_index))
-                #check for spikes kill
-                elif re.search(r'%s\|0 fnt\|\[from\]\s(Spikes)' % dead,t):
-                   dead_to_spikes.append((dead,t_index))
-                #check for poison kill
-                elif re.search(r'%s\|0 fnt\|\[from\]\s(psn)' % dead,t):
-                    dead_to_psn.append((dead,t_index))
-                #check for ability kill
-                elif re.search(r'%s\|0 fnt\|\[from\]\sability:' % dead,t):
-                    killer = re.findall(r'%s\|0 fnt\|\[from\]\sability:.+\[of\]\s(.+)' % dead, t)[0]
-                    kill_list.append(killer)
-                #check for burn/confusion/volatile damage over time kill
-                elif re.search(r'%s\|0 fnt\|\[from\]\s(brn|confusion|%s)' % (dead,'|.+|'.join(volatileDOT)),t):
-                    dead_to_other.append((dead,t_index))
-                #check for Future Sight/Doom Desire kill
-                elif re.search(r'-end\|%s\|move: (Future Sight|Doom Desire)[\s\S]*?%s\|0 fnt' % (dead,dead),t):
-                    killer = re.findall(r'\|move\|(%s.+)\|(?:Future Sight|Doom Desire)' % otherp,rawlog)[-1]
-                    kill_list.append(killer)
-                #otherwise, assume the pokemon that did the killing was whichever other mon clicked a move
-                else:
-                    killer = re.findall(r'\|move\|(%s.+)\|.+\|%s(?!\|\[notarget\])' % (otherp,dead),t)
-                    if killer == []:
-                        killer = re.findall(r'\|-anim\|(%s.+)\|.+\|%s(?!\|\[notarget\])' % (otherp,dead),t)
-                    kill_list.append(killer[0])
-        t_index += 1
-        fainted = re.findall(r'(?<=\|faint\|).+',t)
-        dead_list.extend(fainted)
-    return kill_list, dead_list, dead_to_rocks, dead_to_spikes, dead_to_other, dead_to_psn
+    for i in range(0,2):
+        player_dead = analyze.dead(i)
+        cumulative_deaths.extend(player_dead)
+        dead_list.append(player_dead)
 
-def hazardTurns(player):
-        #define the lists
-        globals()[f'{player}SR'] = []
-        globals()[f'{player}SP'] = []
-        globals()[f'{player}TS'] = []
-        if player == 'p1':
-            opp = 'p2'
-        elif player == 'p2':
-            opp = 'p1'
-        else:
-            return None
-        #stealth rock checks
-        for t in turnlist:
-            if re.search(r'-sidestart\|%s:.+Stealth Rock' % player,t):
-                rocker = re.findall(r'(?<=\|move\|)%sa:.+?(?=\|)' % opp,t)[0]
-                globals()[f'{player}SR'].append(rocker)
-            elif re.search(r'-sideend\|%s.+Stealth Rock' % player,t):
-                globals()[f'{player}SR'].append(rocker)
-                rocker = ''
-            else:
-                try:
-                   globals()[f'{player}SR'].append(rocker)
-                except:
-                    globals()[f'{player}SR'].append('')
-        #spikes checks
-        spikers = []
-        for t in turnlist:
-            if re.search(r'-sideend\|%s:.+\|Spikes' % player,t):
-                spikers.clear()
-            if re.search(r'-sidestart\|%s:.+\|Spikes' % player,t):
-                spike = re.findall(r'(?<=\|move\|)%sa:.+?(?=\|)' % opp,t)[0]
-                spikers.append(spike)
-            globals()[f'{player}SP'].append(list(spikers))
-        #toxic spikes checks
-        tspikers = []
-        for t in turnlist:
-            if re.search(r'-sideend\|%s:.+\|Toxic Spikes' % player,t):
-                tspikers.clear()
-            if re.search(r'-sidestart\|%s:.+\|move: Toxic Spikes' % player,t):
-                try:
-                    tspike = re.findall(r'(?<=\|move\|)%sa:.+?(?=\|)' % opp,t)[0]
-                except:
-                    tspike = re.findall(r'(?<=\|-activate\|)%sa:.+?(?=\|.+Toxic Debris)' % opp,t)[0]
-                tspikers.append(tspike)
-            globals()[f'{player}TS'].append(list(tspikers))
-        return globals()[f'{player}SR'], globals()[f'{player}SP'], globals()[f'{player}TS']
+    for mon in cumulative_deaths:
+        mon_rectified = re.sub(r'^p(\d)[abc]:',r'p\1.:',mon)
+        if mon_rectified not in cumulative_deaths_rectify:
+            cumulative_deaths_rectify.append(mon_rectified)
 
-#second analysis, for awarding indirect kills
-def indirectAward(mode):
-    if mode == 'hazards':
-        for mon in dead_to_rocks:
-            monplayer = mon[0][:2]
-            hazardkiller = globals()[f'{monplayer}SR'][mon[1]]
-            try:
-                kill_list.append(hazardkiller)
-            except:
-                pass
-        for mon in dead_to_spikes:
-            monplayer = mon[0][:2]
-            prespikeHealth = re.findall(r'\d*(?=\/100\n\|-damage\|%s\|0 fnt\|\[from\]\sSpikes)' % mon[0],rawlog)[0]
-            if int(prespikeHealth) < 13:
-                spiker_index = 0
-            elif int(prespikeHealth) < 18:
-                spiker_index = 1
-            else:
-                spiker_index = 2
-            hazardkiller = globals()[f'{monplayer}SP'][mon[1]][spiker_index]
-            try:
-                kill_list.append(hazardkiller)
-            except:
-                pass
-    elif mode == 'psn':
-        for mon in dead_to_psn:
-            monplayer = mon[0][:2]
-            psns = re.findall(r'\|-status\|%s\|(?:psn|tox)(?:\|\[from\].+)?\n' % mon[0],rawlog)
-            latestpsn = re.escape(psns[-1])
-            #toxic orb check
-            if re.search('Toxic Orb',psns[-1]):
-                if re.search(r'\|-item\|%s\|Toxic Orb\|\[from\] move: (?:Trick|Switcheroo)' % mon[0], re.findall(r'[\s\S]+%s' % latestpsn,rawlog)[0]):
-                    #poisoner candidates
-                    psnr_c = re.findall(r'\|-activate\|(.+|%s)\|move: Trick\|\[of\] (%s|.+)' % (mon[0],mon[0]),re.findall(r'[\s\S]+%s' % latestpsn,rawlog)[0])[-1]
-                    psnr = [x for x in psnr_c if x != mon[0]][0]
-                else:
-                    psnr = re.findall(r'\|-item\|%s\|Toxic Orb\|\[from\].+Magician\|\[of\]\s(.+)' % mon[0],re.findall(r'[\s\S]+%s' % latestpsn,rawlog)[0])[-1]
-            else:
-                for t in reversed(turnlist):
-                    if re.search(r'\|switch\|%s\|.+[\s\S]*?%s' % (mon[0],latestpsn),re.findall(r'[\s\S]*?(?=\|move\|)',t)[0]) and len(re.findall(latestpsn,t)) == 1:
-                        finalhp = re.findall(r'\|%s\|.*?(\d*)\/100' % mon[0],rawlog)[-1]
-                        if int(finalhp) <= 13:
-                            tspike_index = 0
-                        else:
-                            tspike_index = -1
-                        psnr = globals()[r'%sTS' % monplayer][mon[1]][tspike_index]
-                        #this determined which tspiker gets the kill
-                    elif re.search(latestpsn,t):
-                        #poisoner candidates
-                        psnr_c = re.findall(r'\|move\|(p[^%s]a: .+)\|.+\|%s' % (monplayer[-1],mon[0]),t)
-                        psnr = [x for x in psnr_c if '|' not in x][0]
-            try:
-                kill_list.append(psnr)
-            except:
-                pass
-    elif mode == 'other':
-        #other includes burn, tricked barbs, salt cure, trapping effects
-        for mon in dead_to_other:
-            monplayer = mon[0][:2]
-            #check for burn
-            if re.search(r'\|-damage\|%s\|0 fnt\|\[from\] brn' % mon[0],rawlog):
-                burns = re.findall(r'\|-status\|%s\|brn(?:\|\[from\].+)?\n' % mon[0],rawlog)
-                latestburn = re.escape(burns[-1])
-                #flame orb check
-                if re.search('Flame Orb',burns[-1]):
-                    if re.search(r'\|-item\|%s\|Flame Orb\|\[from\] move: (?:Trick|Switcheroo)' % mon[0], re.findall(r'[\s\S]+%s' % latestburn,rawlog)[0]):
-                        #burner candidates
-                        burner_c = re.findall(r'\|-activate\|(.+|%s)\|move: Trick\|\[of\] (%s|.+)' % (mon[0],mon[0]),re.findall(r'[\s\S]+%s' % latestburn,rawlog)[0])[-1]
-                        burner = [x for x in burner_c if x != mon[0]][0]
-                    else:
-                        burner = re.findall(r'\|-item\|%s\|Flame Orb\|\[from\].+Magician\|\[of\]\s(.+)' % mon[0],re.findall(r'[\s\S]+%s' % latestburn,rawlog)[0])[-1]
-                else:
-                    for t in reversed(turnlist):
-                        if re.search(latestburn,t):
-                            burner_c = re.findall(r'\|(p[^%s]a: .+)\|.+\|%s' % (monplayer[-1],mon[0]),t)
-                            burner = [x for x in burner_c if '|' not in x][0]
-                kill_list.append(burner)
-            #confusion
-            elif re.search(r'\n\|-damage\|%s\|0 fnt\|\[from\]\sconfusion' % mon[0],turnlist[mon[1]]):
-                latestconf = re.findall(r'\|-start\|%s\|confusion.*' % mon[0],rawlog)[-1]
-                if '[fatigue]' in latestconf:
-                    pass
-                else:
-                    if 'Poison Puppeteer' in latestconf:
-                        killer = re.findall(r'\[of\]\s(.+)',latestconf)[0]
-                    else:
-                        for t in reversed(turnlist):
-                            if re.search(r'%s' % re.escape(latestconf),t):
-                                if re.search(r'\|-heal\|%s\|.+item:.*Berry\n\|-start\|.*confusion' % mon[0],t):
-                                    pass
-                                else:
-                                    killer = re.findall(r'\|move\|(.+)\|.+%s' % mon[0],t)[0]
-                                    break
-                    kill_list.append(killer)
-            #volatile damage-over-time check
-            elif re.search(r'(?<=\n\|-damage\|%s\|0 fnt\|\[from\]\s).+(?!brn|.+Sticky Barb)' % mon[0],turnlist[mon[1]]):
-                widenet = re.findall(r'(?<=\n\|-damage\|%s\|0 fnt\|\[from\]).+' % mon[0],turnlist[mon[1]])[0]
-                try:
-                    smallnet = re.sub(r'\s*?move:\s|\|\[partiallytrapped\]|^\s|\|\[of\].+','',widenet)
-                except:
-                    smallnet = widenet
-                if smallnet in volatileDOT:
-                    for t in turnlist:
-                        #check if a pokemon clicked the killing move on the dead
-                        if re.search(r'\|move\|.+\|%s\|%s\n' % (smallnet, mon[0]),t):
-                            switchflag = False
-                            lastuser = re.findall(r'(?<=\|move\|).+(?=\|%s\|%s)' % (smallnet, mon[0]),t)[0]
-                        #check if the pokemon switched out of the volatile effect
-                        elif re.search(r'\switch\|%s' % mon[0],t):
-                            switchflag = True
-                        else:
-                            pass
-                    #gives the kill to the last user of the volatile move
-                    if switchflag == False:
-                        killer = lastuser
-                        kill_list.append(killer)
-            #check for sticky barb
-            elif re.search(r'\n\|-damage\|%s\|0 fnt\|\[from\]\s.+Sticky Barb' % mon[0],turnlist[mon[1]]):
-                if re.search(r'\|-item\|%s\|Sticky Barb\|\[from\] move: (?:Trick|Switcheroo)' % mon[0],rawlog):
-                    barber_c = re.findall(r'\|-activate\|(.+|%s)\|move: Trick\|\[of\] (%s|.+)' % (mon[0],mon[0]),rawlog)[-1]
-                    barber = [x for x in barber_c if x != mon[0]][0]
-                    kill_list.append(barber)
-                else:
-                    try:
-                        barber = re.findall(r'\|-item\|%s\|Sticky Barb\|\[from\].+Magician\|\[of\]\s(.+)' % mon[0],rawlog)[-1]
-                        kill_list.append(barber)
-                    except:
-                        pass
-            else:
-                pass
+    cumulative_causes = []
+    for mon in cumulative_deaths_rectify:
+        cause_info = analyze.cause_of_death(mon)
+        cumulative_causes.append((mon,cause_info))
 
-def analyzeReplay(r):
-    global kill_list, dead_list, dead_to_rocks, dead_to_spikes, dead_to_other, dead_to_psn, rawlog, turnlist, loglines
-    print(f'Now analyzing: {r}')
-    link = re.sub(r'\s|\?p\d$','',r) + '.log'
-    #get replay log
-    logfile = requests.get(link)
-    rawlog = logfile.text
-    #text cleaning
-    loglines = rawlog.split('\n')
-    turnlist = re.split(r'turn\|\d+',rawlog)
-    turnlist.pop(0)
-    
-    pokeList()
-    p1hazards = hazardTurns('p1')
-    p2hazards = hazardTurns('p2')
-    kill_sources = kill_analyze()
-    #define variables from first analysis
-    kill_list = kill_sources[0]
-    dead_list = kill_sources[1]
-    dead_to_rocks = kill_sources[2]
-    dead_to_spikes = kill_sources[3]
-    dead_to_other = kill_sources[4]
-    dead_to_psn = kill_sources[5]
-    #important variables for second analysis
-    switchflag = True
-    lastuser = ''
+    #print('cmc',cumulative_causes,end='\n\n')
 
-    indirectAward('hazards')
-    indirectAward('psn')
-    indirectAward('other')
+    killer_list = []
+    for ele in cumulative_causes:
+        mon = ele[0]
+        for death_info in ele[1]:
+            flags = death_info[0]
+            dead_turn = death_info[1]
+            killer = kill_award(mon,flags,dead_turn)
+            killer_list.append(killer)
 
-    #kill counting
-    player1name = re.findall(r'\|player\|p1\|(.+)\|.+\n',rawlog)[0]
-    player2name = re.findall(r'\|player\|p2\|(.+)\|.+\n',rawlog)[0]
+    p1_kill_data = []
+    p2_kill_data = []
+    for mon in p1_list:
+        kills = len(re.findall(mon[0],','.join(killer_list)))
+        deaths = len(re.findall(mon[0],','.join(cumulative_deaths)))
+        p1_kill_data.append((mon[1],kills,deaths))
+
+    for mon in p2_list:
+        kills = len(re.findall(mon[0],','.join(killer_list)))
+        deaths = len(re.findall(mon[0],','.join(cumulative_deaths)))
+        p2_kill_data.append((mon[1],kills,deaths))
+
+    # preparing output payload
     printlines = []
+    
     printlines.append('=-=-=-=-=-=-=-=-=-=\n')
-    printlines.append(f'{player1name} vs. {player2name}\n')
-    #p1 kill tally
-    for p in p1pL:
-        kills = sum(1 if p[0] == x else 0 for x in kill_list)
-        deaths = sum(1 if p[0] == x else 0 for x in dead_list)
-        printlines.append(f'{p[1]}\t{kills}\t{deaths}\n')
+    printlines.append(f'{player_names[0]} vs. {player_names[1]}\n')
+    for p in p1_kill_data:
+        printlines.append(f'{p[0]}\t{p[1]}\t{p[2]}\n')
     printlines.append('\n')
-    #p2 kill tally
-    for p in p2pL:
-        kills = sum(1 if p[0] == x else 0 for x in kill_list)
-        deaths = sum(1 if p[0] == x else 0 for x in dead_list)
-        printlines.append(f'{p[1]}\t{kills}\t{deaths}\n')
+    for p in p2_kill_data:
+        printlines.append(f'{p[0]}\t{p[1]}\t{p[2]}\n')
     printlines.append('=-=-=-=-=-=-=-=-=-=\n')
-    with open('output.txt','a') as output:
+    
+    # actually writing the lines to the file
+    with open('output.txt','w') as out:
         for line in printlines:
-            output.write(line)
+            out.write(line)
 
 
+# rudimentary UI
 print('Welcome to the Mass Replay Analyzer')
-print('Would you like to analyze 1 replay, multiple replays from a file, or multiple replays from your clipboard?')
-print('0 - Single Replay\n1 - replays.txt Multiple\n2 - Paste multiple')
-f = open('output.txt','w')
-f.close()
+print('Would you like to analyze 1 replay, or multiple replays from your clipboard?')
+print('0 - Single Replay\n1 - Paste multiple')
+
 while True:
     response = input('')
+
     if response == '0':
-        print('Please paste the replay link here:')
-        clear = open('output.txt','w')
+        print('Please paste the replay link here: ',end='')
         replay = input('')
-        analyzeReplay(replay)
+        replay = re.sub('\n|\?.+$','',replay)
+        analyze_replay(replay)
+        try:
+            analyze_replay(replay)
+        except:
+            rint('An error occured while analyzing')
         break
     elif response == '1':
-        clear = open('output.txt','w')
-        rfile = open('replays.txt')
-        rlist = rfile.readlines()
-        for r in rlist:
-            try:
-                analyzeReplay(r.replace('\n',''))
-            except:
-                print('An error occured')
-        break
-    elif response == '2':
         replays = []
         print('Paste however many replays you\'d like.')
         print('Type "End" to stop pasting replays (not case sensitive)')
@@ -348,14 +106,11 @@ while True:
                 break
             replays.append(line)
         for replay in replays:
+            replay = re.sub('\n|\?.+$','',replay)
             try:
-                analyzeReplay(replay)
+                analyze_replay(replay)
             except:
-                print('An error occured')
+                print('An error occured while analyzing')
         break
     else:
         print('INVALID RESPONSE, please try again')
-
-print('PRESS ENTER TWICE TO CLOSE')
-input('')
-input('')
